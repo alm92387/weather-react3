58591d4d771ef1ca7331d7924661441b3662ae7d
alm92387 <81530707+alm92387@users.noreply.github.com>
2021-06-05 14:35:11 -0400

    removed target blank

--- src/FooterOpenSource.js
+++ src/FooterOpenSource.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function FooterOpenSource (props){
 return ( 
    <div className="footer"> 
- <a href = "https://github.com/alm92387/weather-react3" target="_blank"> Open-source code
+ <a href = "https://github.com/alm92387/weather-react3"> Open-source code
 by Alba Madera
 </a>
 and hosted with <a href ="https://upbeat-snyder-b2e5d8.netlify.app/"> Netlify</a>
