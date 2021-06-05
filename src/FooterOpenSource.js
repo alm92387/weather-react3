bc0f832107028c9aabec34636de64b36a1b6660a
alm92387 <81530707+alm92387@users.noreply.github.com>
2021-06-05 14:33:08 -0400

    added netlify link to footer

--- src/FooterOpenSource.js
+++ src/FooterOpenSource.js
@@ -3,11 +3,11 @@ import React from "react";
 export default function FooterOpenSource (props){
 return ( 
    <div className="footer"> 
-<a href = "https://github.com/alm92387/alba-project" target="">
-Open source coded{" "}
+ <a href = "https://github.com/alm92387/weather-react3" target="_blank"> Open-source code
 by Alba Madera
-{props.href}
 </a>
+and hosted with <a href ="https://upbeat-snyder-b2e5d8.netlify.app/"> Netlify</a>
+
    
   </div>
 );
